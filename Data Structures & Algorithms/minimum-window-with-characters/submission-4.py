539c3153b7e8f8c6565c1c41cb0642e810181c2c
Jacob Baber <jacob.baber@e-hps.com>
2026-08-26 22:28:25 -0500

    Add: minimum-window-with-characters - submission-4

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-4.py
@@ -0,0 +1,37 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        l, r = 0, 0
+        tFreq = {}
+        sFreq = {}
+
+
+        currStr = ""
+        for c in t:
+            tFreq[c] = tFreq.get(c, 0) + 1
+
+        keys = len(tFreq)
+
+        
+
+        while r < len(s):
+            sFreq[s[r]] = sFreq.get(s[r], 0) + 1
+            if sFreq[s[r]] == tFreq.get(s[r]):
+                keys -= 1
+                print(keys)
+
+            r += 1
+
+            while keys == 0:
+                if r - l  < len(currStr) or currStr == "":
+                    currStr = s[l:r]
+                if sFreq[s[l]] == tFreq.get(s[l]):
+                    keys += 1
+                sFreq[s[l]] -= 1
+                if sFreq[s[l]] == 0:
+                    sFreq.pop(s[l])
+                l += 1
+
+        return currStr
+                
+
+        
